b00bf7b58d35375591f71e8fc87b72a14fad8a79
wuyaxuan <w1241419934@163.com>
2021-01-18 17:31:57 +0800

    1st

---
+++ vue-demo1/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <el-container>
+      <el-header>
+        <navHeader />
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import navHeader from './components/nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    navHeader
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+.el-header{
+  padding: 0;
+}
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
